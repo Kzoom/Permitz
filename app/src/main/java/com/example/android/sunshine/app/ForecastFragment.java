8997df2edf2dce353146d05478edd62f82319686
Kelley Willis <kelleywillis99@gmail.com>
2015-03-03 22:57:54 -0800

    first commit on Permitz

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -115,10 +115,8 @@ public class ForecastFragment extends Fragment {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                //ksw...temp toast from Android docs doesn't work, ??getApplicationContext()??
-                // this is from their example
+
                 String forecast = myForecastAdapter.getItem(position);
-                //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
 
                 Intent displayIntent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
                 startActivity(displayIntent);
@@ -129,7 +127,7 @@ public class ForecastFragment extends Fragment {
     }
 
     private void updateWeather(){
-        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        FetchPermitTask weatherTask = new FetchPermitTask();
         //weatherTask.execute("94043");
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String theLocation = sharedPref.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
@@ -142,9 +140,9 @@ public class ForecastFragment extends Fragment {
         updateWeather();
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+    public class FetchPermitTask extends AsyncTask<String, Void, String[]> {
 
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+        private final String LOG_TAG = FetchPermitTask.class.getSimpleName();
 
         //****************************
 
@@ -380,6 +378,214 @@ public class ForecastFragment extends Fragment {
                 for (String dayForecastStr : result){
                     myForecastAdapter.add(dayForecastStr);
                 }  //and that's the new data
+
+        }
+
+
+        //// 3/3/15 ksw...New section for Permitz code
+
+        private String[] getPermitDataFromJson(String permitJsonStr, int numDays)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "list";
+            //final String OWM_WEATHER = "weather";
+            //final String OWM_TEMPERATURE = "temp";
+            //final String OWM_MAX = "max";
+            //final String OWM_MIN = "min";
+
+            final String OWM_ADDR_START = "address_start";
+            final String OWM_ADDR_END = "address_end";
+            final String OWM_STREET = "street_name";
+            final String OWM_STREET_SUFFIX = "street_suffix";
+            final String OWM_VALUE = "valuation";
+            final String ZIP = "zip_code";
+
+            final String OWM_DATETIME = "issue_date";
+            final String OWM_DESCRIPTION = "work_description";
+            final String OWM_LOCATION = "location_1";
+
+            JSONObject permitJson = new JSONObject(permitJsonStr);
+            //JSONArray listArray = permitJson.getJSONArray(OWM_LIST);
+            JSONArray permitArray = permitJson.getJSONArray(OWM_LOCATION);
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < permitArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+
+                // Get the JSON object representing the day
+                JSONObject dayPermit = permitArray.getJSONObject(i);
+
+                // The date/time is returned as a long. We need to convert that
+                // into something human-readable, since most people won't read "1400356800" as
+                // "this saturday".
+                long dateTime = dayPermit.getLong(OWM_DATETIME);
+                day = getReadableDateString(dateTime);
+
+                // description is in a child array called "weather", which is 1 element long.
+                JSONObject weatherObject = dayPermit.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                // Temperatures are in a child object called "temp". Try not to name variables
+                // "temp" when working with temperature. It confuses everybody.
+                JSONObject temperatureObject = dayPermit.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+            for (String s : resultStrs){
+                Log.v(LOG_TAG, "Permits: " + s);
+            }
+
+            return resultStrs;
+        }
+        //***************************
+
+        @Override
+        protected String[] doInBackground2(String... params) {
+
+            if (params.length == 0){
+                //no zip input, nothing to lookup
+                return null;
+            }
+
+            //ksw...Code from their network call snippet
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String permitJsonStr = null;
+
+            String format = "json";
+            String units = "metric";
+            String sinceDate = "'2015-02-27T00:00:00'";
+            int numDays = 7;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+
+                //URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                // 2/11/15 ksw...my pass at URL building, which worked.
+                // 2/17/15 ksw...But theirs is more robust in the long run, so change to it
+               /* Uri.Builder builder = new Uri.Builder();
+                builder.scheme("http")
+                        .authority("api.openweathermap.org")
+                        .appendPath("data")
+                        .appendPath("2.5")
+                        .appendPath("forecast")
+                        .appendPath("daily")
+                        .appendQueryParameter("q",params[0])
+                        .appendQueryParameter("mode","json")
+                        .appendQueryParameter("units","metric")
+                        .appendQueryParameter("cnt","7");
+
+                URL url = new URL(builder.build().toString());
+                Log.v(LOG_TAG, "Build URL: " + url.toString());
+                */
+
+                // 3/3/15 ksw...build SoQL query for Permits
+                final String PERMIT_BASE_URL = "https://data.lacity.org/resource/yv23-pmwf.json?";
+                final String SELECT_COLS = "$select=zip_code, issue_date, address_start, address_end, street_name, street_suffix, work_description, valuation"
+                final String QUERY_PARAM = " AND zip_code = ";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "&$where=issue_date >= ";
+
+                //You can declare all this the above way or even inside the Uri.parse() and appendQueryParameter()
+
+                Uri builtUri = Uri.parse(PERMIT_BASE_URL).buildUpon()
+                        .appendQueryParameter(SELECT_COLS)
+                        //.appendQueryParameter(FORMAT_PARAM, format)  //json
+                        //.appendQueryParameter(UNITS_PARAM, units)    //metric
+                        .appendQueryParameter(DAYS_PARAM, sinceDate)
+                        .appendQueryParameter(QUERY_PARAM, params[0])  //zip_code
+                        .build();
+
+                //At last
+
+                URL url = new URL(builtUri.toString());
+                Log.v(LOG_TAG, "Build URL: " + url.toString());
+
+
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null; // their code was forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    return null; // their code was forecastJsonStr = null;
+                }
+                permitJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Permit JSON string: " + permitJsonStr);
+
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error on Xoom", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                return null; // their code was forecastJsonStr = null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream on Xoom", e);
+                    }
+                }
+            }
+
+            try {
+                return getPermitDataFromJson(permitJsonStr, numDays);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute2(String[] result) {
+            if (result != null)
+                myForecastAdapter.clear();
+            for (String dayPermitStr : result){
+                myForecastAdapter.add(dayPermitStr);
+            }  //and that's the new data
 
         }
     }
